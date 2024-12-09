7546796565c20c9a96829d21c39739a80398302b
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-09 15:15:09 +0545

    update orderRepository

--- src/main/java/com/dipakraut/eCommerce/repository/order/OrderRpository.java
+++ src/main/java/com/dipakraut/eCommerce/repository/order/OrderRpository.java
@@ -1,2 +1,7 @@
-package com.dipakraut.eCommerce.repository.order;public interface OrderRpository {
+package com.dipakraut.eCommerce.repository.order;
+
+import com.dipakraut.eCommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRpository extends JpaRepository<Order, Long> {
 }
