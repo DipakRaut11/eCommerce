54554335f1db479111ee76dae6d995be44c45b3c
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:27:54 +0545

    update userService interface

--- src/main/java/com/dipakraut/eCommerce/service/user/IUserService.java
+++ src/main/java/com/dipakraut/eCommerce/service/user/IUserService.java
@@ -1,2 +1,13 @@
-package com.dipakraut.eCommerce.service.user;public interface IUserService {
+package com.dipakraut.eCommerce.service.user;
+
+import com.dipakraut.eCommerce.model.User;
+import com.dipakraut.eCommerce.request.user.CreateUserRequest;
+import com.dipakraut.eCommerce.request.user.UpdateUserRequest;
+
+public interface IUserService {
+    User getUserById(long userId);
+    User createUser(CreateUserRequest request);
+    User updateUser(UpdateUserRequest request, long userId);
+    void deleteUser(long userId);
+
 }
