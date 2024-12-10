abad5c24c0e11e37a180ca3c92cc93497e5856f7
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:25:46 +0545

    update orderService interface

--- src/main/java/com/dipakraut/eCommerce/service/order/IOrderService.java
+++ src/main/java/com/dipakraut/eCommerce/service/order/IOrderService.java
@@ -2,8 +2,12 @@ package com.dipakraut.eCommerce.service.order;
 
 import com.dipakraut.eCommerce.model.Order;
 
+import java.util.List;
+
 public interface IOrderService {
 
     Order placeOrder(Long userId);
     Order getOrderById(Long orderId);
+
+    List<Order> getUserOrder(Long userId);
 }
