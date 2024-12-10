1533910da343aef6841ae9c6e5d8115f07ae94a0
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:26:13 +0545

    update orderService

--- src/main/java/com/dipakraut/eCommerce/service/order/OrderService.java
+++ src/main/java/com/dipakraut/eCommerce/service/order/OrderService.java
@@ -8,11 +8,13 @@ import com.dipakraut.eCommerce.model.OrderItem;
 import com.dipakraut.eCommerce.model.Product;
 import com.dipakraut.eCommerce.repository.order.OrderRpository;
 import com.dipakraut.eCommerce.repository.product.ProductRepository;
+import com.dipakraut.eCommerce.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -21,16 +23,27 @@ public class OrderService implements IOrderService {
 
     private final OrderRpository orderRpository;
     private final ProductRepository productRepository;
+    private final CartService cartService;
 
 
     @Override
     public Order placeOrder(Long userId) {
-        return null;
+        Cart cart = cartService.getCartByUserId(userId);
+
+        Order order = createOrder(cart);
+        List<OrderItem> orderItemList = createOrerItems(order, cart);
+
+        order.setOrderItems(new HashSet<>(orderItemList));
+        order.setTotalAmount(calculateTotalAmount(orderItemList));
+        Order saveOrder = orderRpository.save(order);
+        cartService.clearCart(cart.getId());
+
+        return saveOrder;
     }
 
     private Order createOrder(Cart cart) {
         Order order = new Order();
-        //set user ....
+        order.setUser(cart.getUser());
         order.setOrderStatus(OderStatus.PENDING);
         order.setOrderDate(LocalDate.now());
         return order;
@@ -65,5 +78,10 @@ public class OrderService implements IOrderService {
     public Order getOrderById(Long orderId) {
         return orderRpository.findById(orderId)
                 .orElseThrow(()->new ResourceNotFoundException("Order not found"));
+    }
+
+    @Override
+    public List<Order> getUserOrder(Long userId) {
+        return orderRpository.findByUserId(userId);
     }
 }
