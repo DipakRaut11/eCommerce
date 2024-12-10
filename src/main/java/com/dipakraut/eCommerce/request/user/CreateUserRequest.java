ad0b7a9139fc99729a0f60d3e059c0e92bebbe28
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:23:16 +0545

    crate product request

--- src/main/java/com/dipakraut/eCommerce/request/user/CreateUserRequest.java
+++ src/main/java/com/dipakraut/eCommerce/request/user/CreateUserRequest.java
@@ -1,2 +1,14 @@
-package com.dipakraut.eCommerce.request.user;public class CreateUserRequest {
+package com.dipakraut.eCommerce.request.user;
+
+import lombok.Data;
+import org.hibernate.annotations.NaturalId;
+
+@Data
+public class CreateUserRequest {
+
+    private String firstName;
+    private String lastName;
+    @NaturalId
+    private String email;
+    private String password;
 }
