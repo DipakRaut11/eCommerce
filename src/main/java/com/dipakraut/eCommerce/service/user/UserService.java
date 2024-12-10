91b5e3d9dc00e1fb7ae81495a0148f6cc0bea37a
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:28:16 +0545

    update userService

--- src/main/java/com/dipakraut/eCommerce/service/user/UserService.java
+++ src/main/java/com/dipakraut/eCommerce/service/user/UserService.java
@@ -1,2 +1,45 @@
-package com.dipakraut.eCommerce.service.user;public class UserService {
+package com.dipakraut.eCommerce.service.user;
+
+import com.dipakraut.eCommerce.exception.ResourceNotFoundException;
+import com.dipakraut.eCommerce.model.User;
+import com.dipakraut.eCommerce.repository.user.UserRepository;
+import com.dipakraut.eCommerce.request.user.CreateUserRequest;
+import com.dipakraut.eCommerce.request.user.UpdateUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(UpdateUserRequest request, long userId) {
+        return userRepository.findById(userId)
+                .map(existingUser ->{
+                    existingUser.setFirstName(request.getFirstName());
+                    existingUser.setLastName(request.getLastName());
+                    return userRepository.save(existingUser);
+                }).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+    }
+
+    @Override
+    public void deleteUser(long userId) {
+        userRepository.findById(userId)
+                .ifPresentOrElse(userRepository :: delete, () ->{
+                    throw new ResourceNotFoundException("User not found with id: " + userId);
+                });
+    }
 }
