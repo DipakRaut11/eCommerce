504ca32df18080f52fd25aeca36df83fc712cea3
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:19:23 +0545

    modify order repository

--- src/main/java/com/dipakraut/eCommerce/repository/order/OrderRpository.java
+++ src/main/java/com/dipakraut/eCommerce/repository/order/OrderRpository.java
@@ -3,5 +3,9 @@ package com.dipakraut.eCommerce.repository.order;
 import com.dipakraut.eCommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRpository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
 }
