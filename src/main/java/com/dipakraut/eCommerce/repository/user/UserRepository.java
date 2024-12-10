73addfd52883f7c1cb813211425bc7da1469b62e
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-10 06:20:16 +0545

    modify user repository

--- src/main/java/com/dipakraut/eCommerce/repository/user/UserRepository.java
+++ src/main/java/com/dipakraut/eCommerce/repository/user/UserRepository.java
@@ -1,2 +1,7 @@
-package com.dipakraut.eCommerce.repository.user;public interface UserRepository {
+package com.dipakraut.eCommerce.repository.user;
+
+import com.dipakraut.eCommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
