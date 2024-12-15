4bfa09f8523a4800693624a8c58e69c189e47c94
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-15 15:04:18 +0545

    add data initializer

--- src/main/java/com/dipakraut/eCommerce/data/DataInitializerr.java
+++ src/main/java/com/dipakraut/eCommerce/data/DataInitializerr.java
@@ -1,2 +1,41 @@
-package com.dipakraut.eCommerce.data;public class DataInitializerr {
+package com.dipakraut.eCommerce.data;
+
+import com.dipakraut.eCommerce.model.User;
+import com.dipakraut.eCommerce.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializerr implements ApplicationListener<ApplicationReadyEvent> {
+    private final UserRepository userRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        createDefaultUserIfNotExists();
+
+    }
+
+    private void createDefaultUserIfNotExists(){
+        for (int i = 1; i<=5; i++){
+            String defaultEmail = "user"+i+"@gmail.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("The user");
+            user.setLastName("User" +i);
+            user.setEmail(defaultEmail);
+            user.setPassword("123456");
+            userRepository.save(user);
+            System.out.println("Default vet user "+i+"create successfully.");
+
+        }
+
+
+    }
+
 }
