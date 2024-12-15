5439b335c1d9fb319a028ec9532d4b9f075b5b18
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-15 14:59:51 +0545

    update order repository

--- src/main/java/com/dipakraut/eCommerce/repository/order/OrderRepository.java
+++ src/main/java/com/dipakraut/eCommerce/repository/order/OrderRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface OrderRpository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByUserId(Long userId);
 }
