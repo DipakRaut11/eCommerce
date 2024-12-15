1ccebdb7d5aa2f08e52d52d2bbaa9380f84e3839
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-15 14:57:12 +0545

    update order service interface

--- src/main/java/com/dipakraut/eCommerce/service/order/IOrderService.java
+++ src/main/java/com/dipakraut/eCommerce/service/order/IOrderService.java
@@ -1,5 +1,6 @@
 package com.dipakraut.eCommerce.service.order;
 
+import com.dipakraut.eCommerce.dto.order.OrderDto;
 import com.dipakraut.eCommerce.model.Order;
 
 import java.util.List;
@@ -7,7 +8,10 @@ import java.util.List;
 public interface IOrderService {
 
     Order placeOrder(Long userId);
-    Order getOrderById(Long orderId);
+    OrderDto getOrderById(Long orderId);
 
-    List<Order> getUserOrder(Long userId);
+    List<OrderDto> getUserOrders(Long userId);
+
+
+    OrderDto convertToDto(Order order);
 }
