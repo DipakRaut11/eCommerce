645d24a7e20dcbad7a82a6e17f92be901e7b2791
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-22 14:08:50 +0545

    update, create default user and admin

--- src/main/java/com/dipakraut/eCommerce/data/DataInitializer.java
+++ src/main/java/com/dipakraut/eCommerce/data/DataInitializer.java
@@ -1,5 +1,6 @@
 package com.dipakraut.eCommerce.data;
 
+import com.dipakraut.eCommerce.model.Role;
 import com.dipakraut.eCommerce.model.User;
 import com.dipakraut.eCommerce.repository.role.RoleRepository;
 import com.dipakraut.eCommerce.repository.user.UserRepository;
@@ -8,13 +9,14 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
+@Transactional
 @Component
 @RequiredArgsConstructor
-public class DataInitializerr implements ApplicationListener<ApplicationReadyEvent> {
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
@@ -22,11 +24,17 @@ public class DataInitializerr implements ApplicationListener<ApplicationReadyEve
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
         createDefaultUserIfNotExists();
+        createDefaultRoleIfNotExists(defaultRoles);
+        createDefaultAdminIfNotExists();
 
     }
 
     private void createDefaultUserIfNotExists(){
+
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+
         for (int i = 1; i<=5; i++){
             String defaultEmail = "user"+i+"@gmail.com";
             if (userRepository.existsByEmail(defaultEmail)){
@@ -37,20 +45,43 @@ public class DataInitializerr implements ApplicationListener<ApplicationReadyEve
             user.setLastName("User" +i);
             user.setEmail(defaultEmail);
             user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
             System.out.println("Default vet user "+i+"create successfully.");
 
         }
 
-        private void createDefaultRoleIfNotExist(Set<String> roles) {
-            roles.stream()
-                    .filter(role -> roleRepository.findByName(role).isEmpty())
-                    .map(Role::new)
-                    .forEach(roleRepository::save);
+    }
+    private void createDefaultAdminIfNotExists(){
+
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+
+        for (int i = 1; i<=2; i++){
+            String defaultEmail = "admin"+i+"@gmail.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("Admin" +i);
+            user.setEmail(defaultEmail);
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+            System.out.println("Default admin user "+i+"create successfully.");
+
         }
 
-
-
     }
+
+    private void createDefaultRoleIfNotExists(Set<String> roles){
+        roles.stream()
+                .filter(role -> roleRepository.findByName(role).isEmpty())
+                .map(Role:: new)
+                .forEach(roleRepository::save);
+    }
+
+
+
 
 }
