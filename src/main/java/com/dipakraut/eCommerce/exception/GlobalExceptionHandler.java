4be98d82405a3d0bd8dcfc9977d324dae32396ad
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-24 14:22:36 +0545

    addes global exception handler

--- src/main/java/com/dipakraut/eCommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dipakraut/eCommerce/exception/GlobalExceptionHandler.java
@@ -1,2 +1,21 @@
-package com.dipakraut.eCommerce.exception;public class GlobalExceptionHandler {
+package com.dipakraut.eCommerce.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(
+            AccessDeniedException.class
+    )
+    public ResponseEntity<String>
+    handleAccessDeniedException(AccessDeniedException ex){
+        String message = "You dont have access to this resource";
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
 }
