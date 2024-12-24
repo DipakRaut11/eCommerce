c6abf17c10afe8a4c23405a59575d929741af8a1
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-24 14:23:06 +0545

    addes login request

--- src/main/java/com/dipakraut/eCommerce/request/security/LoginRequest.java
+++ src/main/java/com/dipakraut/eCommerce/request/security/LoginRequest.java
@@ -1,2 +1,13 @@
-package com.dipakraut.eCommerce.request.security;public class LoginRequest {
+package com.dipakraut.eCommerce.request.security;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
 }
