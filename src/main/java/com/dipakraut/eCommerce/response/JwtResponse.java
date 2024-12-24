4cc4d66d04a1ffbf0e08697364732c4c91d5a437
Dipak Raut <dipakkumarraut1119@gmail.com>
2024-12-24 14:23:40 +0545

    addes Jwt response

--- src/main/java/com/dipakraut/eCommerce/response/JwtResponse.java
+++ src/main/java/com/dipakraut/eCommerce/response/JwtResponse.java
@@ -1,2 +1,13 @@
-package com.dipakraut.eCommerce.response;public class JwtResponse {
+package com.dipakraut.eCommerce.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private Long id;
+    private String token;
 }
